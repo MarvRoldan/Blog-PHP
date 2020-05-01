adc5d2fa9042fdd575f84bc975fd73932b608219
MarvRoldan <marvinroldan_@hotmail.com>
2020-04-30 20:20:44 -0600

    Added the Article Class

--- includes/Articles.Class.php
+++ includes/Articles.Class.php
@@ -1,20 +1,32 @@
 <?php
 
-class Articles 
+class Articles
 {
-    private $allArticles= array();
+    private $allArticles = array();
 
-    function __construct( $jsonFilePath = '')
+    function __construct($jsonFilePath = '')
     {
-        if ( file_exists( $jsonFilePath ) )
+        if (file_exists($jsonFilePath)) 
         {
-            $jsonString = file_get_contents( $jsonFilePath );
+            $jsonString = file_get_contents($jsonFilePath);
 
-            $jsonObject = json_decode( $jsonString );
+            $jsonObject = json_decode($jsonString);
 
-            if ( is_array( $jsonObject->articles ) )
-            {
+            if (is_array($jsonObject->articles)) {
                 $this->allArticles = $jsonObject->articles;
+            }
+        }
+    }
+
+    public function output ()
+    {
+        if ( is_array( $this->allArticles ) && !empty ($this->allArticles))
+        {
+            echo'<h2>Articles<h2>';
+
+            foreach ( $this->allArticles as $articles )
+            {
+                $newArticle = new 
             }
         }
     }
